d32bd6f805f8c318ab2d339ed8af7a0c01e172f4
Kaashvi Das <kaashvidas06@gmail.com>
2026-07-19 18:07:24 +0530

    Create inOrder.cpp
    Given the root of a binary tree, return the inorder traversal of its nodes' values.

---
+++ binary-tree/inOrder.cpp
@@ -0,0 +1,41 @@
+/*
+Problem: Binary Tree Inorder Traversal
+Platform: LeetCode #94
+Difficulty: Easy
+
+Approach:
+1. Use the recursive function yto first call the right child, push the current val in the answer array and then call for left child.
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+
+Date Solved: 2026-07-19
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void inOrder(TreeNode* root, vector <int>&ans){
+        if (root==nullptr){
+            return;
+        }
+        inOrder(root->left, ans);
+        ans.push_back(root->val);
+        inOrder(root->right, ans);
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector <int> ans;
+        inOrder(root, ans);
+        return ans;        
+    }
+};
